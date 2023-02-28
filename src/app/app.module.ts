32a8a3a5500a3ce6ce4d0607c48cb16229ec9139
Ralph Renzo Pitogo <ralphrenzo96@gmail.com>
2023-02-28 21:17:15 +0800

    Implemented Material design.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,15 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/common/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
 
 const appRoutes : Routes = [
   { path : '', component: TasksComponent },
@@ -37,9 +46,20 @@ const appRoutes : Routes = [
     FontAwesomeModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes, { enableTracing : true })
+    RouterModule.forRoot(appRoutes, { enableTracing : true }),
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [
+     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance : 'outline' } }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
